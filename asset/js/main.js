9f6fd6a9fb735ea327108557599b1539e3c62637
FreelancerMamun <mamunkhan16345@gmail.com>
2024-07-05 02:58:09 +0600

    update

--- asset/js/main.js
+++ asset/js/main.js
@@ -17,3 +17,34 @@ closeElement.addEventListener('click', (e) => {
   e.preventDefault();
   menuTogglee.classList.remove('active');
 });
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '50px',
+  slidesToShow: 3,
+  arrows: true,
+  nextArrow:
+    '<button class="nextArrow"><i class="bi bi-arrow-right-short"></i></button>',
+  prevArrow:
+    '<button class="prevArrow"><i class="bi bi-arrow-left-short"></i></button>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
